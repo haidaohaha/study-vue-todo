0f8475c734b4a8cd6b515fa40034c3925cd2ad97
孙猛 <sunmengwork@foxmail.com>
2019-03-22 15:19:32 +0800

    feat:简单todolist

--- README.md
+++ README.md
@@ -1,39 +1,8 @@
-# my-vue
-
-## Project setup
-
-```
-yarn install
-```
-
-### Compiles and hot-reloads for development
-
-```
-yarn run serve
-```
-
-### Compiles and minifies for production
-
-```
-yarn run build
-```
-
-### Run your tests
-
-```
-yarn run test
-```
-
-### Lints and fixes files
-
-```
-yarn run lint
-```
-
-### Customize configuration
-
-See [Configuration Reference](https://cli.vuejs.org/config/).
-
 ### 现在才开始学 VUE 哈哈
 
 #### 开个分支 dev
+
+#### 刷视频制作 todolist
+
+[vue2.5 入门](https://www.imooc.com/learn/980)  
+DellLee 大佬，坐标阿里南京
---
+++ src/components/TodoList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="todolist">
+    <div class="green" :title="title">{{ msg }}</div>
+    <input v-model="inputValue">
+    <button @click="handleSubmit">提交</button>
+    <ul>
+      <li v-for="(item,index) in todoList" :key="index">{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todolist",
+  props: {
+    msg: String
+  },
+  methods: {
+    handleSubmit: function() {
+      if (!this.inputValue) return;
+      this.todoList.push(this.inputValue);
+      this.inputValue = "";
+    }
+  },
+  data() {
+    return { title: "this is todolist", inputValue: "", todoList: [] };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.green {
+  color: #42b983;
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif;
+  font-size: 30px;
+  font-weight: 600;
+  margin-bottom: 20px;
+}
+</style>
